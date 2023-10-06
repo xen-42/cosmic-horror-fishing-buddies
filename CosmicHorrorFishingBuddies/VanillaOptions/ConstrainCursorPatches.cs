39cca71af51157fc9462dba2ddb4f0db60e521d4
Nick <nicholasconnors@hotmail.ca>
2023-10-06 00:22:08 -0400

    Fix constrained cursor

---
+++ CosmicHorrorFishingBuddies/VanillaOptions/ConstrainCursorPatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CosmicHorrorFishingBuddies.VanillaOptions;
+
+[HarmonyPatch]
+internal class ConstrainCursorPatches
+{
+	[HarmonyPostfix]
+	[HarmonyPatch(typeof(DredgeInputManager), nameof(DredgeInputManager.RefreshCursorLockState))]
+	public static void DredgeInputManager_RefreshCursorLockState()
+	{
+		// Should make this it's own mod
+		// When the cursor is invisible it should be confined no matter what (i.e., when driving boat around)
+		if (!Cursor.visible && Cursor.lockState == CursorLockMode.None)
+		{
+			Cursor.lockState = CursorLockMode.Confined;
+		}
+	}
+}
