4850a4b49ec7b9db76af3b113e6e6770ab81decd
Nick <nicholasconnors@hotmail.ca>
2023-10-06 20:36:59 -0400

    Add other helper classes

---
+++ CosmicHorrorFishingBuddies/Util/AbilityHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmicHorrorFishingBuddies.Util;
+
+public static class AbilityHelper
+{
+	private static readonly Dictionary<Type, Ability> _abilityCache = new();
+	private static readonly Dictionary<Type, string> _abilityNames = new()
+	{
+		{ typeof(LightAbility), "lights" },
+		{ typeof(TeleportAbility), "manifest" },
+		{ typeof(AtrophyAbility), "atrophy" },
+		{ typeof(BaitAbility), "bait" },
+		{ typeof(DeployPotAbility), "pot" },
+		{ typeof(TrawlNetAbility), "trawl" },
+		{ typeof(BoostAbility), "haste" },
+		{ typeof(FoghornAbility), "foghorn" },
+		{ typeof(SpyglassAbility), "spyglass" },
+
+	};
+
+	public static Ability GetAbility(Type type)
+	{
+		if (!_abilityCache.ContainsKey(type))
+		{
+			_abilityCache.Add(type, GameManager.Instance.PlayerAbilities.abilityMap[_abilityNames[type]]);
+		}
+		return _abilityCache[type];
+	}
+
+	public static T GetAbility<T>() where T : Ability
+	{
+		return GetAbility(typeof(T)) as T;
+	}
+}
