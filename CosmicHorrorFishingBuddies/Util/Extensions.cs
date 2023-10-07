110f11794406d58c34e925ec87c10d8ae58fe60c
Nick <nicholasconnors@hotmail.ca>
2023-10-06 20:35:19 -0400

    Add extensions from messages

---
+++ CosmicHorrorFishingBuddies/Util/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using CosmicHorrorFishingBuddies.Core;
+using Mirror;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CosmicHorrorFishingBuddies.Util;
+
+public static class Extensions
+{
+	public static IEnumerable<Type> GetDerivedTypes(this Type type) =>
+		type.Assembly
+			.GetTypes()
+			.Where(x => !x.IsInterface && !x.IsAbstract && type.IsAssignableFrom(x))
+			.OrderBy(x => x.FullName);
+
+	public static NetworkConnectionToClient GetNetworkConnection(this uint playerId)
+	{
+		var conn = NetworkServer.connections.Values.FirstOrDefault(x => playerId == x.GetPlayerId());
+		if (conn == default)
+		{
+			CFBCore.LogError($"GetNetworkConnection on {playerId} found no connection\n{Environment.StackTrace}");
+		}
+
+		return conn;
+	}
+
+	public static uint GetPlayerId(this NetworkConnectionToClient conn)
+	{
+		if (conn.identity == null)
+		{
+			CFBCore.LogError($"GetPlayerId on {conn} has no identity.");
+			return uint.MaxValue;
+		}
+
+		return conn.identity.netId;
+	}
+}
