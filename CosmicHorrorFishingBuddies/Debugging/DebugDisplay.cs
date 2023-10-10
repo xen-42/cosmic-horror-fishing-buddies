7fd6fc0ab16f3c55928f0a4032af25e3c8d0c2dd
Nick <nicholasconnors@hotmail.ca>
2023-10-10 00:02:38 -0400

    Add debug info on screen

---
+++ CosmicHorrorFishingBuddies/Debugging/DebugDisplay.cs
@@ -0,0 +1,22 @@
+﻿using CosmicHorrorFishingBuddies.Core;
+using CosmicHorrorFishingBuddies.PlayerSync;
+using TMPro;
+using UnityEngine;
+
+namespace CosmicHorrorFishingBuddies.Debugging
+{
+	internal class DebugDisplay : MonoBehaviour
+	{
+		private TextMeshProUGUI _text;
+
+		public void Awake()
+		{
+			_text = gameObject.AddComponent<TextMeshProUGUI>();
+		}
+
+		public void Update()
+		{
+			_text.text = $"{(CFBCore.IsHost ? "host" : "client")} - {NetworkPlayer.LocalPlayer?.netId}";
+		}
+	}
+}
